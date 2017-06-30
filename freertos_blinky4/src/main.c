6e138241487eeae5d36888eac08c862804dda056
Gustavo Muro <gustmuro@gmail.com>
2017-06-30 19:41:55 -0300

    add example 4

---
+++ freertos_blinky4/src/main.c
@@ -0,0 +1,118 @@
+/* Copyright 2016, Eric Pernia.
+ * Copyright 2017, Gustavo Muro.
+ * All rights reserved.
+ *
+ * This file is part sAPI library for microcontrollers.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * Date: 2016-04-26
+ */
+
+/*==================[inclusions]=============================================*/
+
+#include "switches_leds.h"   /* <= own header */
+
+#include "sapi.h"            /* <= sAPI header */
+
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+static void task(void * a)
+{
+   /* Variable para almacenar el valor de tecla leido */
+   bool_t valor;
+
+   /* ------------- REPETIR POR SIEMPRE ------------- */
+   while(1)
+   {
+      vTaskDelay(10 / portTICK_RATE_MS);
+
+      valor = !gpioRead( TEC1 );
+      gpioWrite( LEDB, valor );
+
+      valor = !gpioRead( TEC2 );
+      gpioWrite( LED1, valor );
+
+      valor = !gpioRead( TEC3 );
+      gpioWrite( LED2, valor );
+
+      valor = !gpioRead( TEC4 );
+      gpioWrite( LED3, valor );
+   }
+}
+
+/*==================[external functions definition]==========================*/
+
+/* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
+int main(void){
+
+   /* ------------- INICIALIZACIONES ------------- */
+
+   /* Inicializar la placa */
+   boardConfig();
+
+   /* Inicializar GPIOs */
+   gpioConfig( 0, GPIO_ENABLE );
+
+   /* Configuración de pines de entrada para Teclas de la EDU-CIAA-NXP */
+   gpioConfig( TEC1, GPIO_INPUT );
+   gpioConfig( TEC2, GPIO_INPUT );
+   gpioConfig( TEC3, GPIO_INPUT );
+   gpioConfig( TEC4, GPIO_INPUT );
+
+   /* Configuración de pines de salida para Leds de la EDU-CIAA-NXP */
+   gpioConfig( LEDR, GPIO_OUTPUT );
+   gpioConfig( LEDG, GPIO_OUTPUT );
+   gpioConfig( LEDB, GPIO_OUTPUT );
+   gpioConfig( LED1, GPIO_OUTPUT );
+   gpioConfig( LED2, GPIO_OUTPUT );
+   gpioConfig( LED3, GPIO_OUTPUT );
+
+   xTaskCreate(task, (const char *)"task1", configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+1, NULL);
+
+   vTaskStartScheduler();
+
+	return 0 ;
+}
+
+/*==================[end of file]============================================*/
